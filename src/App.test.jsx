29f27ba66dc1939b47c0f343845f1976e0286909
VolTorian <33360939+VolTorian@users.noreply.github.com>
2025-02-25 01:34:29 -0800

    tests navbar buttons
    make sure they link to the correct pages

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from 'react-router-dom';
 
 import App from "./App";
 
@@ -8,5 +10,38 @@ describe("App component", () => {
         const { container } = render(<App />);
 
         expect(container).toMatchSnapshot();
+    });
+
+    it("home button sends to home page", async () => {
+        const user = userEvent.setup();
+
+        render(<App />);
+
+        const homeButton = screen.getByRole("button", { name: "Home"});
+
+        await user.click(homeButton);
+        expect(screen.getByRole("heading").textContent).toMatch(/Home/i);
+    });
+
+    it("shop button sends to shop page", async () => {
+        const user = userEvent.setup();
+
+        render(<App />);
+
+        const shopButton = screen.getByRole("button", { name: "Shop"});
+
+        await user.click(shopButton);
+        expect(screen.getByRole("heading").textContent).toMatch(/Shop/i);
+    });
+
+    it("cart button sends to cart page", async () => {
+        const user = userEvent.setup();
+
+        render(<App />);
+
+        const cartButton = screen.getByRole("button", { name: "Cart"});
+
+        await user.click(cartButton);
+        expect(screen.getByRole("heading").textContent).toMatch(/Cart/i);
     });
 });
