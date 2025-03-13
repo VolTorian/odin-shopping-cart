c2aca3b05d5c3b1ff3cdbb99b9d7186a7e13e4bf
VolTorian <33360939+VolTorian@users.noreply.github.com>
2025-03-13 00:23:38 -0700

    App fetches data from fake store api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
 import './App.css'
 
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router';
 
 import NavBar from "./NavBar";
 
 function App() {
+    const SHOP_ITEM_COUNT = 20;
+    const [shopItems, setShopItems] = useState([]);
+
+    useEffect(() => {
+        fetchItems(SHOP_ITEM_COUNT);
+    }, []);
+
+    async function fetchItems(numberOfItems) {
+        const initialList = new Array(numberOfItems).fill(null);
+        setShopItems(initialList);
+
+        for (let i = 1; i <= numberOfItems; i++) {
+            fetch(`https://fakestoreapi.com/products/${i}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    setShopItems((prev) => {
+                        const updated = [...prev];
+                        updated[i - 1] = data;
+                        // console.log(data.id)
+                        return updated;
+                    });
+                });
+        };
+    }
+
     return (
         <>
             <NavBar />
-            <Outlet />
+            <Outlet context={[shopItems]} />
         </>
     );
 }
 
-export default App
+export default App;
