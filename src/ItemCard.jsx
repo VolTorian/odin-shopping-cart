9b763f0efdda5c98fa6cf9a030c75e8f720c6336
VolTorian <33360939+VolTorian@users.noreply.github.com>
2025-03-19 21:42:27 -0700

    general shop item card layout

--- src/ItemCard.jsx
+++ src/ItemCard.jsx
@@ -2,8 +2,12 @@ function ItemCard({ data }) {
 
     return (
         <div className="item-card">
-            <h3>{data.id}</h3>
-            <h6>{data.title}</h6>
+            {/* <h3>{data.id}</h3> */}
+            <div className="image-container">
+                <img src={data.image} alt={"Image for " + data.title} />
+            </div>
+            <h4 className="item-title">{data.title}</h4>
+            <div className="item-rating">{data.rating.rate}/5 out of {data.rating.count} ratings</div>
         </div>
     )
 }
