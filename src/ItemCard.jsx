53731dce5e8e2964d96728c25903a7ca7bbd2b6f
VolTorian <33360939+VolTorian@users.noreply.github.com>
2025-04-22 21:12:30 -0700

    fixed controlled input issue with amount number

--- src/ItemCard.jsx
+++ src/ItemCard.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
 function ItemCard({ data }) {
+    const [amount, setAmount] = useState(1);
+
+    const handleAmountChange = (e) => {
+        const newAmount = e.target.value;
+
+        if (newAmount === "") {
+            setAmount(1);
+        }
+        else if (parseInt(newAmount) >= 1) {
+            setAmount(parseInt(newAmount));
+        }
+    };
 
     return (
         <div className="item-card">
@@ -10,7 +24,7 @@ function ItemCard({ data }) {
             <div className="item-rating">{data.rating.rate}/5 out of {data.rating.count} ratings</div>
             <div className="item-card-buttons">
                 <button>Details</button>
-                <input type="number" step="1" min="1" value="1" />
+                <input type="number" step="1" min="0" value={amount} onChange={handleAmountChange}/>
                 <button>Add to Cart</button>
             </div>
         </div>
