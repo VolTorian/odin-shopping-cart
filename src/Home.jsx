9fd48516c8858e5d28ff4056a1016b2d6e1eac23
VolTorian <33360939+VolTorian@users.noreply.github.com>
2025-05-11 15:35:06 -0700

    short home page message

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,10 @@
 function Home () {
     return (
-        <h3>Home</h3>
+        <div>
+            <h3>Home</h3>
+            <div>A simple mock online store made with React and Fake Store API, created as part of The Odin Project.</div>
+            <div>Explore the source code <a href="https://github.com/VolTorian/odin-shopping-cart">here</a>.</div>
+        </div>
     );
 }
 
